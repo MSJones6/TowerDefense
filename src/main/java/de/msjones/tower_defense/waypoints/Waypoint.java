54b3cad1be7e69cb49b36b0963ed2f7e95bc8bef
Indiana Jones <MSJones@gmx.net>
2023-12-03 23:36:35 +0100

    equals and hasCode Method for Waypoint class

--- src/main/java/de/msjones/tower_defense/waypoints/Waypoint.java
+++ src/main/java/de/msjones/tower_defense/waypoints/Waypoint.java
@@ -3,9 +3,24 @@ package de.msjones.tower_defense.waypoints;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.Objects;
+
 @Data
 @AllArgsConstructor
 public class Waypoint {
     private double x;
     private double y;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Waypoint waypoint = (Waypoint) o;
+        return Double.compare(x, waypoint.x) == 0 && Double.compare(y, waypoint.y) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
 }
